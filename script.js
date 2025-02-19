1cdfd055892d968a26f0b47112021701907ee133
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2025-02-19 16:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+const nameInput = document.getElementById('name');
+const yearInput = document.getElementById('year');
+const submitBtn = document.getElementById('button');
+const url = document.getElementById('url');
+
+submitBtn.addEventListener('click', () => {
+	const name = nameInput.value;
+	const year = yearInput.value;
+	if(name && year) {
+		url.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+	}
+	else if(name) {
+		url.textContent = `https://localhost:8080/?name=${name}`;
+	}
+	else if(year) {
+		url.textContent = `https://localhost:8080/?year=${year}`
+	}
+	else {
+		url.textContent = `https://localhost:8080/`;
+	}
+	nameInput.value = '';
+	yearInput.value = '';
+})
